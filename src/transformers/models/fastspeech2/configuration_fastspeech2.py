b190551084739aea9e741c230f8535a55c76d5f4
Jake Tae <jaesungtae@gmail.com>
2022-01-20 17:58:19 +0000

    feature: update config w/ relevant attr

--- src/transformers/models/fastspeech2/configuration_fastspeech2.py
+++ src/transformers/models/fastspeech2/configuration_fastspeech2.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" FastSpeech2 model configuration """
+""" FastSpeech2 model configuration"""
 
 from ...configuration_utils import PretrainedConfig
 from ...utils import logging
@@ -28,49 +28,41 @@ FASTSPEECH2_PRETRAINED_CONFIG_ARCHIVE_MAP = {
 
 class FastSpeech2Config(PretrainedConfig):
     r"""
-    This is the configuration class to store the configuration of a [`~FastSpeech2Model`].
-    It is used to instantiate an FastSpeech2 model according to the specified arguments, defining the model
-    architecture. Instantiating a configuration with the defaults will yield a similar configuration to that of
-    the FastSpeech2 [microsoft/fastspeech2](https://huggingface.co/microsoft/fastspeech2) architecture.
+    This is the configuration class to store the configuration of a [`~FastSpeech2Model`]. It is used to instantiate an
+    FastSpeech2 model according to the specified arguments, defining the model architecture. Instantiating a
+    configuration with the defaults will yield a similar configuration to that of the FastSpeech2
+    [microsoft/fastspeech2](https://huggingface.co/microsoft/fastspeech2) architecture.
 
-    Configuration objects inherit from  [`PretrainedConfig`] and can be used
-    to control the model outputs. Read the documentation from  [`PretrainedConfig`]
-    for more information.
+    Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
+    documentation from [`PretrainedConfig`] for more information.
 
 
     Args:
         vocab_size (`int`, *optional*, defaults to 30522):
-            Vocabulary size of the FastSpeech2 model. Defines the number of different tokens that can be represented by the
-            `inputs_ids` passed when calling [`~FastSpeech2Model`] or
-            [`~TFFastSpeech2Model`].
-        hidden_size (`int`, *optional*, defaults to 768):
-            Dimension of the encoder layers and the pooler layer.
-        num_hidden_layers (`int`, *optional*, defaults to 12):
-            Number of hidden layers in the Transformer encoder.
+            Vocabulary size of the FastSpeech2 model. Defines the number of different tokens that can be represented by
+            the `inputs_ids` passed when calling [`~FastSpeech2Model`] or [`~TFFastSpeech2Model`].
+        hidden_size (`int`, *optional*, defaults to 256):
+            Dimension of the Transformer encoder layers.
+        num_encoder_layers (`int`, *optional*, defaults to 4):
+            Number of hidden layers prior to the Length Regulator.
+        num_decoder_layers (`int`, *optional*, defaults to 4):
+            Number of hidden layers after the Length Regulator.
         num_attention_heads (`int`, *optional*, defaults to 12):
             Number of attention heads for each attention layer in the Transformer encoder.
         intermediate_size (`int`, *optional*, defaults to 3072):
             Dimension of the "intermediate" (i.e., feed-forward) layer in the Transformer encoder.
         hidden_act (`str` or `function`, *optional*, defaults to `"gelu"`):
-            The non-linear activation function (function or string) in the encoder and pooler.
-            If string, `"gelu"`, `"relu"`, `"selu"` and `"gelu_new"` are supported.
+            The non-linear activation function (function or string) in the encoder and pooler. If string, `"gelu"`,
+            `"relu"`, `"selu"` and `"gelu_new"` are supported.
         hidden_dropout_prob (`float`, *optional*, defaults to 0.1):
             The dropout probabilitiy for all fully connected layers in the embeddings, encoder, and pooler.
         attention_probs_dropout_prob (`float`, *optional*, defaults to 0.1):
             The dropout ratio for the attention probabilities.
         max_position_embeddings (`int`, *optional*, defaults to 512):
-            The maximum sequence length that this model might ever be used with.
-            Typically set this to something large just in case (e.g., 512 or 1024 or 2048).
-        type_vocab_size (`int`, *optional*, defaults to 2):
-            The vocabulary size of the `token_type_ids` passed when calling [`~FastSpeech2Model`] or
-            [`~TFFastSpeech2Model`].
-        initializer_range (`float`, *optional*, defaults to 0.02):
-            The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
+            The maximum sequence length that this model might ever be used with. Typically set this to something large
+            just in case (e.g., 512 or 1024 or 2048).
         layer_norm_eps (`float`, *optional*, defaults to 1e-12):
             The epsilon used by the layer normalization layers.
-        use_cache (`bool`, *optional*, defaults to `True`):
-            Whether or not the model should return the last key/values attentions (not used by all models). Only
-            relevant if `config.is_decoder=True`.
         Example:
 
     ```python
@@ -84,50 +76,41 @@ class FastSpeech2Config(PretrainedConfig):
 
     >>> # Accessing the model configuration
     >>> configuration = model.config
-    ```
-"""
+    ```"""
     model_type = "fastspeech2"
-    
 
     def __init__(
         self,
-        vocab_size=30522,
-        hidden_size=768,
-        num_hidden_layers=12,
-        num_attention_heads=12,
-        intermediate_size=3072,
+        vocab_size=79,
+        hidden_size=256,
+        num_encoder_layers=4,
+        num_decoder_layers=4,
+        kernel_size=9,
+        num_attention_heads=2,
+        intermediate_size=1024,
         hidden_act="gelu",
         hidden_dropout_prob=0.1,
-        attention_probs_dropout_prob=0.1,
-        max_position_embeddings=512,
-        type_vocab_size=2,
+        attention_probs_dropout_prob=0.0,
+        predictor_probs_dropout_prob=0.5,
+        max_position_embeddings=2000,
         initializer_range=0.02,
-        layer_norm_eps=1e-12,
-        use_cache=True,
-        is_encoder_decoder=False,
-        pad_token_id=1,
-        bos_token_id=0,
-        eos_token_id=2,
+        layer_norm_eps=1e-5,
+        pad_token_id=0,
         **kwargs
     ):
+        if kernel_size % 2 == 0:
+            raise ValueError(f"Expected `kernel_size` to be odd, but got {kernel_size}")
         self.vocab_size = vocab_size
         self.max_position_embeddings = max_position_embeddings
         self.hidden_size = hidden_size
-        self.num_hidden_layers = num_hidden_layers
+        self.num_encoder_layers = num_encoder_layers
+        self.num_decoder_layers = num_decoder_layers
+        self.kernel_size = kernel_size
         self.num_attention_heads = num_attention_heads
         self.intermediate_size = intermediate_size
         self.hidden_act = hidden_act
         self.hidden_dropout_prob = hidden_dropout_prob
         self.attention_probs_dropout_prob = attention_probs_dropout_prob
-        self.initializer_range = initializer_range
-        self.type_vocab_size = type_vocab_size
+        self.predictor_probs_dropout_prob = predictor_probs_dropout_prob
         self.layer_norm_eps = layer_norm_eps
-        self.use_cache = use_cache
-        super().__init__(
-            pad_token_id=pad_token_id,
-            bos_token_id=bos_token_id,
-            eos_token_id=eos_token_id,
-            **kwargs
-        )
-
-    
+        super().__init__(pad_token_id=pad_token_id, **kwargs)
